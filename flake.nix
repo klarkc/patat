a9275c6b76b86b6ebb52e6e761bad3cf68c608d7
Walker <walker@praiseweb.com.br>
2023-01-19 16:56:18 -0300

    Add nix support

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  # This is a template created by `hix init`
+  inputs.haskell-nix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskell-nix/nixpkgs-unstable";
+  inputs.utils.url = "github:ursi/flake-utils";
+  outputs = { self, utils, ... }@inputs:
+    utils.apply-systems
+      {
+        inherit inputs;
+        # TODO support additional systems
+        #  right now we can't afford to test every other system
+        systems = [ "x86_64-linux" "aarch64-linux" ];
+        overlays = [ inputs.haskell-nix.overlay ];
+      }
+      ({ pkgs, system, ... }:
+        let
+          hixProject = pkgs.haskell-nix.hix.project {
+            src = ./.;
+            evalSystem = "x86_64-linux";
+          };
+          flake = hixProject.flake { };
+        in
+        {
+          # Flake definition follows hello.cabal
+          inherit (flake) checks devShells hydraJobs;
+          legacyPackages = pkgs;
+          packages = flake.packages // {
+            default = self.packages.${system}."patat:lib:patat";
+          };
+          apps = flake.apps // {
+            default = self.apps.${system}."patat:exe:patat";
+          };
+        });
+
+  # --- Flake Local Nix Configuration ----------------------------
+  nixConfig = {
+    # This sets the flake to use nix cache.
+    # Nix should ask for permission before using it,
+    # but remove it here if you do not want it to.
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://cache.iog.io"
+      "https://klarkc.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "klarkc.cachix.org-1:R+z+m4Cq0hMgfZ7AQ42WRpGuHJumLLx3k0XhwpNFq9U="
+    ];
+    allow-import-from-derivation = "true";
+  };
+}
